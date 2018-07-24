6609a22f569c9317dcd4eb647274558c47cfd1b0
irwan cigist <cigist@users-MacBook-Pro.local>
2018-07-24 19:47:39 +0700

    ignore node modules git

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import greet from './js/hello.js';
+import './style.css'
+
+console.log("I'm the entry point");
+greet();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+      app:'./src/index.js',
+      print:'./src/js/greeter.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        title: 'webpack-example',
+        template: './src/index.html'
+    })
+    // new ExtractTextPlugin({ // define where to save the file
+    //     filename: 'dist/css/[name].[chunkhash].css'
+    // })
+  ],
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+    module: {
+         rules: [
+           {
+             test: /\.css$/,
+             use: [
+               'style-loader',
+               'css-loader'
+             ]
+           }
+         ]
+    }
+};
